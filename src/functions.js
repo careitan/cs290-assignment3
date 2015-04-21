27277af82d22179875152d49c044ece63f19c19d
Allan Reitan <careitan2000@gmail.com>
2015-04-20 21:37:33 -0700

    Again

--- src/functions.js
+++ src/functions.js
@@ -108,7 +108,7 @@ function parseGit(gitArray) {
     tempValue3 = gitArray[i].substr(gitArray[i].indexOf('\"'),
       (gitArray[i].length - 1) - gitArray[i].indexOf('\"') + 2);
 
-    tempValue3 = tempValue3.substr(1,tempValue3.indexOf('\"'));
+    /*tempValue3 = tempValue3.substr(1,tempValue3.indexOf('\"'));*/
 
     tempValue = new GitLog(tempValue1, tempValue2, tempValue3);
 
